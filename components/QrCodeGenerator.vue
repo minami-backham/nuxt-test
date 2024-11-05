30330f96c0aea0be17391ace4f0838dcac0c02a7
minami-backham <saeki@backham.me>
2024-11-05 16:06:23 +0900

    ログイン画面：QRコード生成

---
+++ components/QrCodeGenerator.vue
@@ -0,0 +1,36 @@
+// QrCodeGenerator.vue
+<template>
+  <div class="flex flex-col items-center">
+    <canvas ref="qrCanvas"></canvas>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, watch, onMounted } from "vue";
+import QRCode from "qrcode";
+
+interface Props {
+  url: string;
+}
+
+const props = defineProps<Props>();
+const qrCanvas = ref<HTMLCanvasElement | null>(null);
+
+const generateQRCode = async (url: string) => {
+  if (qrCanvas.value && url) {
+    await QRCode.toCanvas(qrCanvas.value, url);
+  }
+};
+
+// URLが更新されるたびにQRコードを再生成する
+watch(
+  () => props.url,
+  (newUrl) => {
+    if (newUrl) generateQRCode(newUrl);
+  }
+);
+
+onMounted(() => {
+  if (props.url) generateQRCode(props.url);
+});
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col items-center">
+    <h1 class="text-2xl font-bold">ログイン画面</h1>
+    <QrCodeGenerator :url="url" />
+    <div>6桁コード表示</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import QrCodeGenerator from "~/components/QrCodeGenerator.vue";
+
+const url = ref<string>("");
+
+onMounted(() => {
+  url.value = "https://www.google.com/";
+});
+</script>
