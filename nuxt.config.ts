b7544676a957e0a024c5ec799ba908fb0a1a389d
minami-backham <saeki@backham.me>
2024-11-12 18:47:27 +0900

    TypeScriptの設定を変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,11 @@ import { defineNuxtConfig } from "nuxt/config";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: "2024-11-06",
   devtools: { enabled: true },
   typescript: {
-    strict: true, // 型チェックを有効化
-  },
-  css: ["@/assets/css/tailwind.css"], // Tailwind CSSを追加
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
+    shim: false, // 型定義ファイルを生成しない
+    strict: true, // 厳格な型チェックを有効化
+    typeCheck: true, // 型チェックを有効化
   },
 });
